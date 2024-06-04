158c082b945243d7222e1d1883b2d75075268022
Wang, Yanyao <yanyao.wang@amd.com>
2024-06-04 09:21:30 -0500

    update dependency tree for stage2 components

--- rocm-build/ccache-env-mathlib.sh
+++ rocm-build/ccache-env-mathlib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set ccache environment variable for math libraries
-if [ "${CCACHE_ENABLED}" == "true" ] && [ -f "${OUT_DIR}"/logs/rocm-dev ]; then
+if [ "${CCACHE_ENABLED}" == "true" ] && [ -f "${OUT_DIR}"/logs/lightning ]; then
     export LAUNCHER_FLAGS="-DCMAKE_CXX_COMPILER_LAUNCHER=/usr/local/bin/ccache -DCMAKE_C_COMPILER_LAUNCHER=/usr/local/bin/ccache"
     export CCACHE_IGNOREHEADERS=${ROCM_PATH}/include:${ROCM_PATH}/llvm/include:${ROCM_PATH}/lib/llvm/lib/clang/17/include:${ROCM_PATH}/lib/llvm/include
     export CCACHE_COMPILERCHECK=none
