2253584c0d6e0da43c3d5d7a7f159130537d7632
Wang, Yanyao <yanyao.wang@amd.com>
2024-12-10 12:14:00 -0800

    set correct required build scripts

---
+++ tools/rocm-build/build_transferbench.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+source "$(dirname "${BASH_SOURCE[0]}")/compute_utils.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/compute_helper.sh"
+
+set_component_src TransferBench
+
+build_transferbench() {
+    echo "Start build"
+
+    if [ "${ENABLE_STATIC_BUILDS}" == "true" ]; then
+        ack_and_skip_static
+    fi
+
+    mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR"
+    init_rocm_common_cmake_params
+
+    CXX="$ROCM_PATH"/bin/hipcc \
+    cmake "${rocm_math_common_cmake_params[@]}" "$COMPONENT_SRC"
+    make package
+
+    rm -rf _CPack_Packages/ && find -name '*.o' -delete
+    copy_if "DEB" "${CPACKGEN:-"deb;rpm"}" "${PACKAGE_DIR}" "${BUILD_DIR}"/*."${PKGTYPE}"
+    copy_if "RPM" "${CPACKGEN:-"deb;rpm"}" "${PACKAGE_DIR}" "${BUILD_DIR}"/*."${PKGTYPE}"
+    show_build_cache_stats
+}
+
+clean_transferbench() {
+    echo "Cleaning TransferBench build directory: ${BUILD_DIR} ${PACKAGE_DIR}"
+    rm -rf "$BUILD_DIR" "$PACKAGE_DIR"
+    echo "Done!"
+}
+
+stage2_command_args "$@"
+
+case $TARGET in
+    build) build_transferbench; build_wheel ;;
+    outdir) print_output_directory ;;
+    clean) clean_transferbench ;;
+    *) die "Invalid target $TARGET" ;;
+esac
